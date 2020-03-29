6a9a7d34fe8182693240ad9b932f7de09a0ccd10
lewisgarton <lewisgarton@gmail.com>
2020-03-29 21:14:59 +1300

    Create Product model class

---
+++ WingtipToys/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WingtipToys.Models
+{
+    public class Product
+    {
+        [ScaffoldColumn(false)]
+        public int ProductID { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Name")]
+        public String ProductName { get; set; }
+
+        [Required, StringLength(10000), Display(Name = "Product Desctiption")]
+        public String Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        [Display(Name = "Price")]
+        public double? UnitPrice { get; set; }
+
+        public int? CatagoryID { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
